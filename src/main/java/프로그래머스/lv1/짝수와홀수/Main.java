f75f80fa10055dae0d1b48929d9863cbada4636b
dojuun2 <djp9816@gmail.com>
2023-03-10 00:51:33 +0900

    [프로그래머스] 짝수와 홀수

--- src/main/java/프로그래머스/lv1/짝수와홀수/Main.java
+++ src/main/java/프로그래머스/lv1/짝수와홀수/Main.java
@@ -1,5 +1,9 @@
 package 프로그래머스.lv1.짝수와홀수;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     // 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환
     public String solution(int num) {
@@ -13,7 +17,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int num = 4;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = 0;
+        try {
+            num = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         Main m = new Main();
         String result = m.solution(num);
         System.out.println(result);
